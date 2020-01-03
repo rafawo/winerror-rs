1a8065380077852b6be24fcb9863743d7336484f
rafawo <rafawo1@hotmail.com>
2020-01-02 17:29:04 -0800

    Add .gitignore, licenses, readme and stub library

---
+++ README.md
@@ -0,0 +1,6 @@
+# winerror-rs
+Rust wrapper of Windows Error codes defined in [winerror.h](https://docs.microsoft.com/en-us/windows/win32/debug/system-error-codes).
+
+## Creates.io version notes
+
+This section briefly describes all published crates.io [versions](https://crates.io/crates/winerror-rs/versions) of this project, ordered from latest to oldest.
---
+++ src/lib.rs
@@ -0,0 +1,61 @@
+//! Rust abstractions of windows error code definitions for the Win32 API functions.
+//!
+//! Values are 32 bit values layed out as follows:
+//! <pre>
+//!  3 3 2 2 2 2 2 2 2 2 2 2 1 1 1 1 1 1 1 1 1 1
+//!  1 0 9 8 7 6 5 4 3 2 1 0 9 8 7 6 5 4 3 2 1 0 9 8 7 6 5 4 3 2 1 0
+//! +---+-+-+-----------------------+-------------------------------+
+//! |Sev|C|R|     Facility          |               Code            |
+//! +---+-+-+-----------------------+-------------------------------+
+//!
+//! where
+//!
+//!     Sev - is the severity code
+//!
+//!         00 - Success
+//!         01 - Informational
+//!         10 - Warning
+//!         11 - Error
+//!
+//!     C - is the Customer code flag
+//!
+//!     R - is a reserved bit
+//!
+//!     Facility - is the facility code
+//!
+//!     Code - is the facility's status code
+//! </pre>
+//!
+//! HRESULTs are 32 bit values layed out as follows:
+//! <pre>
+//!  3 3 2 2 2 2 2 2 2 2 2 2 1 1 1 1 1 1 1 1 1 1
+//!  1 0 9 8 7 6 5 4 3 2 1 0 9 8 7 6 5 4 3 2 1 0 9 8 7 6 5 4 3 2 1 0
+//! +-+-+-+-+-+---------------------+-------------------------------+
+//! |S|R|C|N|r|    Facility         |               Code            |
+//! +-+-+-+-+-+---------------------+-------------------------------+
+//!
+//! where
+//!
+//!     S - Severity - indicates success/fail
+//!
+//!         0 - Success
+//!         1 - Fail (COERROR)
+//!
+//!     R - reserved portion of the facility code, corresponds to NT's
+//!             second severity bit.
+//!
+//!     C - reserved portion of the facility code, corresponds to NT's
+//!             C field.
+//!
+//!     N - reserved portion of the facility code. Used to indicate a
+//!             mapped NT status value.
+//!
+//!     r - reserved portion of the facility code. Reserved for internal
+//!             use. Used to indicate HRESULT values that are not status
+//!             values, but are instead message ids for display strings.
+//!
+//!     Facility - is the facility code
+//!
+//!     Code - is the facility's status code
+//! </pre>
+//!
